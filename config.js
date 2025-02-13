2645062a1e77a4fc9ea263c2f5e14bea8003e47e
Yejisseu <91386545+Yejisseu@users.noreply.github.com>
2025-02-13 16:54:50 +0800

    config.js

--- config.js
+++ config.js
@@ -78,10 +78,10 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dpj3bekfr/video/upload/v1739436766/yohlixwom2xoozp0kcc9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
